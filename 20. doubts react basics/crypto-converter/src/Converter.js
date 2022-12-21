f987baaa5464d15cd2a041e5405afc6efd613d62
Amir Sohail <amirsohail335@gmail.com>
2022-12-21 10:25:13 +0530

    committed

---
+++ 20. doubts react basics/crypto-converter/src/Converter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card } from 'antd'
+
+function Converter() {
+  return (
+    <div className='container'>
+        <Card className="crypto-card" title={<h1>Crypto Converter</h1>}>
+
+        </Card>
+        
+    </div>
+  )
+}
+
+export default Converter
