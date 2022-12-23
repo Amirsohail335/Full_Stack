086081fa24c75ab70bfb967863903f9feaceddda
Amir Sohail <amirsohail335@gmail.com>
2022-12-23 08:34:49 +0530

    committed

--- 22. introduction to redux/redux-app/src/redux/slices/messageSlice.js
+++ 22. introduction to redux/redux-app/src/redux/slices/messageSlice.js
@@ -1,5 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const messageSlice = createSlice({
-  name: "messageSLice",
-});
+    name: 'messageSlice',
+    initialState: {
+        message: '',
+        counter: 0
+    },
+    reducers: {
+        setMessage: (state, action) => {
+            state.message = action.payload;
+        },
+        resetMessage: (state, action) => {
+            state.message = ''
+        }
+    }
+})
+
+export default messageSlice.reducer;
+
+export const { setMessage, resetMessage } = messageSlice.actions
