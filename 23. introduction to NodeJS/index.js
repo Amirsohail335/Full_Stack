1d4beb8844ffecbcea52cef5f7aabb3896bba4ba
Amir Sohail <amirsohail335@gmail.com>
2022-12-26 14:25:02 +0530

    committed

--- 23. introduction to NodeJS/index.js
+++ 23. introduction to NodeJS/index.js
@@ -1,2 +1,11 @@
- 
-console.log(multiply(2,3));
+const express = require("express");
+
+const app = express();
+
+app.get("/user", (req, res) => {
+  console.log("user was called");
+});
+
+app.listen(4000, () => {
+  console.log("listening to port:4000");
+});
