718a6785a76d7e13e68e75f9897e33be0a3af1b8
Amir Sohail <amirsohail335@gmail.com>
2022-12-30 14:51:14 +0530

    node

--- 23. introduction to NodeJS/index.js
+++ 23. introduction to NodeJS/index.js
@@ -4,6 +4,22 @@ const app = express();
 
 app.get("/user", (req, res) => {
   console.log("user was called");
+
+  res.send({
+    name:"Amir",
+    age: 24,
+  });
+});
+
+app.get("/", (req, res) => {
+    // res.send("server is now working");
+    // res.sendFile(__dirname + "/index.html");
+    res.json({
+        success: true,
+    });
+    // res.send({
+    //     success: true,
+    // });
 });
 
 app.listen(4000, () => {
