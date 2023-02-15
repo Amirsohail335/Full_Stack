303f1423532aa9ba2e20eb1be9e81638443211b8
Amir Sohail <amirsohail335@gmail.com>
2023-02-15 08:35:02 +0530

    committed

--- 21. react router and navigation/routing-app/src/pages/Pricing.js
+++ 21. react router and navigation/routing-app/src/pages/Pricing.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet, useLocation } from "react-router";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function Pricing() {
 
--- 21. react router and navigation/routing-app/src/pages/Profile.js
+++ 21. react router and navigation/routing-app/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import { joinPaths } from "@remix-run/router";
+// import { joinPaths } from "@remix-run/router";
 import React, { useEffect } from "react";
 import { useParams } from "react-router";
 
